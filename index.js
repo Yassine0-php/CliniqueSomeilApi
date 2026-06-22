df4701c3b79c42f5a41ec9528ed928c921355c15
yassine Bessaim <yassinebessaim@gmail.com>
2026-06-22 15:17:55 +0200

    initialisation avec angular

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 
 const app = express();
+const cors = require('cors');
+
+app.use(cors({
+    origin: "http://localhost:4200/"
+}));
 
 app.use(express.json());
 
