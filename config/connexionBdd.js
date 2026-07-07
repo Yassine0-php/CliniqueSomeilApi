619ebaabdbfe4267c472352ed6866f15f5b86bab
yassine Bessaim <yassinebessaim@gmail.com>
2026-07-07 09:40:11 +0200

    suppression affichage info connexion

--- config/connexionBdd.js
+++ config/connexionBdd.js
@@ -16,10 +16,7 @@ bdd.connect((err) => {
     }
 
      console.log("Connexion MySQL réussie !");
-     console.log(process.env.DB_HOST);
-     console.log(process.env.DB_USER);
-     console.log(process.env.DB_PASSWORD);
-     console.log(process.env.DB_NAME);
+    
 });
 
 
